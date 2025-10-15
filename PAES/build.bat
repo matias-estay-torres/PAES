a38c4575bf256d92e6fbe719f18ffc75523392f2
Matías Estay Torres <mestay@utem.cl>
2025-10-15 01:50:12 -0300

    Delete PAES directory

--- PAES/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Compila paes_processor en Windows usando g++ (MSYS2/Mingw en PATH)
-g++ -std=c++11 -O3 -fopenmp -Wall -Wextra -o paes_processor paes_processor_v2.cpp
-if %ERRORLEVEL% neq 0 (
-    echo Compilación fallida.
-    pause
-    exit /b %ERRORLEVEL%
-)
-echo Compilación exitosa: paes_processor.exe
